19767d1dad3821bbc06ed5cb3f0f6c1b26db2e28
Jagadeesh Jami <jaggu1232000@gmail.com>
2024-04-30 10:23:44 +0530

    Implemented Clock config screen

---
+++ src/main/java/com/board/config/boardconfiggui/datamodels/ClockConfigModel.java
@@ -0,0 +1,43 @@
+package com.board.config.boardconfiggui.datamodels;
+
+public class ClockConfigModel {
+    private ViewType viewType;
+    private String label;
+    private String result;
+
+    public ClockConfigModel(ViewType viewType, String label, String result) {
+        this.viewType = viewType;
+        this.label = label;
+        this.result = result;
+    }
+
+    public ViewType getViewType() {
+        return viewType;
+    }
+
+    public void setViewType(ViewType viewType) {
+        this.viewType = viewType;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public enum ViewType {
+        TEXT_FIELD,
+        RADIO_BUTTON
+    }
+}
+
