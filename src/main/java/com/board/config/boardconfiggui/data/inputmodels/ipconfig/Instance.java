f0e3c458ca01fc2fb40024d22ed8536448dbaac6
Sujana2422 <86820188+Sujana2422@users.noreply.github.com>
2024-04-30 15:01:15 +0530

    Added input models & initialize method to get the input config from the XML files (#3)
    * Added models for ipconfig and pinconfig
    
    * Added InputConfigRepo and updated models
    
    * Minor changes
    
    * Added hascode & equals methods for Pinconfig
    
    * Addressed review comments
    
    * Merge branch 'dev' into sbokku-test

---
+++ src/main/java/com/board/config/boardconfiggui/data/inputmodels/ipconfig/Instance.java
@@ -0,0 +1,58 @@
+package com.board.config.boardconfiggui.data.inputmodels.ipconfig;
+
+import com.board.config.boardconfiggui.data.Constants;
+import com.board.config.boardconfiggui.data.enums.DeviceRole;
+
+import javax.xml.bind.annotation.*;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Represents an IP instance with list of parameters.
+ */
+@XmlRootElement(name = "Instance")
+public class Instance {
+    @XmlAttribute(name = "name")
+    String name;
+    @XmlElement(name = "param")
+    List<Parameter> parameters;
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Parameter> getParameters() {
+        return parameters;
+    }
+
+    public DeviceRole getDeviceRole() {
+        for (Parameter parameter : parameters) {
+            if (Constants.DEV_ROLE.equals(parameter.getName())) {
+                return DeviceRole.valueOf(parameter.getValue().toUpperCase());
+            }
+        }
+        // Default to null if devRole parameter is not found
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Instance instance = (Instance) o;
+        return Objects.equals(name, instance.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Instance{" +
+                "name='" + name + '\'' +
+                ", parameters=" + parameters +
+                '}';
+    }
+}
