e32c6813b7f1eb1d67cab907ae7ae46b8502be55
sbokku <sujanasri.bokku@invecas.com>
2024-05-06 19:39:50 +0530

    Added list of params dynamically

--- src/main/java/com/board/config/boardconfiggui/SPIIpConfigController.java
+++ src/main/java/com/board/config/boardconfiggui/SPIIpConfigController.java
@@ -1,5 +1,6 @@
 package com.board.config.boardconfiggui;
 
+import com.board.config.boardconfiggui.controllers.OnOffButtonWidgetController;
 import com.board.config.boardconfiggui.data.Constants;
 import com.board.config.boardconfiggui.data.IpPinConfig;
 import com.board.config.boardconfiggui.data.inputmodels.pinconfig.Pin;
@@ -7,28 +8,36 @@ import com.board.config.boardconfiggui.data.inputmodels.pinconfig.Port;
 import com.board.config.boardconfiggui.data.repo.InputConfigRepo;
 import com.board.config.boardconfiggui.interfaces.BoardPageDataSaverInterface;
 import com.board.config.boardconfiggui.ui.models.SPIConfigModel;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import org.apache.commons.lang3.StringUtils;
 
 import java.net.URL;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class SPIIpConfigController implements Initializable, BoardPageDataSaverInterface {
     private final String ipName;
     private List<SPIConfigModel> spiConfigModels;
 
     @FXML
-    private Pane spiConfigGridPane;
+    private OnOffButtonWidgetController onOffWidgetController;
+    @FXML
+    private GridPane gridpaneWidget;
 
     public SPIIpConfigController(String ipName) {
         this.ipName = ipName;
@@ -41,6 +50,21 @@ public class SPIIpConfigController implements Initializable, BoardPageDataSaverI
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        onOffWidgetController.setButtonLabel("Enable Status:");
+        onOffWidgetController.getButton().setOnAction(actionEvent -> {
+            if (StringUtils.equals(OnOffButtonWidgetController.OFF_TXT, ((Button) actionEvent.getSource()).getText())) {
+                onOffWidgetController.setButtonTextColor(Color.valueOf("#008000"));
+                onOffWidgetController.setButtonText("ON");
+                gridpaneWidget.setVisible(true);
+            } else {
+                onOffWidgetController.setButtonTextColor(Color.valueOf("#ff0000"));
+                onOffWidgetController.setButtonText("OFF");
+                gridpaneWidget.setVisible(false);
+                //clear data
+            }
+
+        });
+
         InputConfigRepo inputConfigRepo = InputConfigRepo.getInstance();
         List<Port> ports = inputConfigRepo.getPinConfig().getPorts();
 
@@ -69,38 +93,39 @@ public class SPIIpConfigController implements Initializable, BoardPageDataSaverI
             });
         }
 
-        GridPane gridPane = new GridPane();
-        gridPane.setAlignment(Pos.CENTER);
-        gridPane.setPadding(new Insets(60));
-        gridPane.setVgap(40); // Set vertical gap between rows
-        gridPane.setHgap(40);
+//        GridPane gridPane = new GridPane();
+//        gridpaneWidget.setAlignment(Pos.CENTER);
+        gridpaneWidget.setPadding(new Insets(60));
+        gridpaneWidget.setVgap(40); // Set vertical gap between rows
+        gridpaneWidget.setHgap(40);
 
         int col = 0;
         int row = 0;
         for (SPIConfigModel spiConfigModel : spiConfigModels) {
             String label = spiConfigModel.getLabel();
 
-            Label labelControl = new Label(label + ":");
-            Font myFont = Font.font("", FontWeight.BOLD, 14);
-            labelControl.setFont(myFont);
+            Label labelControl = new Label(label);
+            gridpaneWidget.add(labelControl, col, row);
 
-            gridPane.add(labelControl, col, row);
+            ComboBox comboBox = new ComboBox();
+            List<String> displayValues = spiConfigModel.getIpPinConfigs()
+                    .stream()
+                    .map(IpPinConfig::getDisplayValue)
+                    .collect(Collectors.toList());
+            comboBox.setItems(FXCollections.observableArrayList(displayValues));
+            comboBox.setPromptText(Constants.SELECT);
+            gridpaneWidget.add(comboBox, col + 1, row);
 
-            TextField textField = new TextField();
-            textField.setText(spiConfigModel.getResult());
-            gridPane.add(textField, col + 1, row);
-            // Store user-entered value into result field
-            textField.textProperty().addListener((observable, oldValue, newValue) -> {
-                spiConfigModel.setResult(newValue);
+            comboBox.itemsProperty().addListener((observable, oldValue, newValue) -> {
+                spiConfigModel.setResult(comboBox.getSelectionModel().getSelectedItem().toString());
             });
 
             col += 2;
-            if (col >= 6) {
+            if (col >= 4) {
                 col = 0;
                 row++;
             }
         }
 
-        spiConfigGridPane.getChildren().add(gridPane);
     }
 }
